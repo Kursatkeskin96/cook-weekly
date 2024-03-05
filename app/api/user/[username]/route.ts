b0f3f976d4305be34a5b2815d01b481742d76748
Kursat Keskin <kursatkeskinn@gmail.com>
2024-03-05 11:39:58 +0000

    Dynamic User API route added

---
+++ app/api/user/[username]/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server';
+import {db} from '@/lib/db'; // Ensure this path points to where your Prisma instance is initialized
+
+export async function GET(req: Request, context: any ) {
+  const { params } = context
+
+  try {
+  const username = await db.user.findUnique({
+    where: {
+      username: params.username
+    },
+    select: {
+      id: true,
+      username: true,
+      email: true,
+    }
+  })
+  if(!username) {
+    return NextResponse.json({username: null, message: 'Username couldnt found'}, {status: 404})
+  }
+  return NextResponse.json({username}, {status: 200})
+} catch (error) {
+  return NextResponse.json({username: null, message: 'something went wrong'}, {status: 500})
+}
+}
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import {hash} from 'bcrypt'
 
+import { NextApiRequest, NextApiResponse } from 'next';
+
 export async function POST(req: Request) {
    try {
     const body = await req.json();
@@ -36,4 +38,23 @@ export async function POST(req: Request) {
    } catch (error) {
     return NextResponse.json({message: "Something went wrong"}, {status: 500});
    }
+}
+
+export async function GET() {
+    try {
+      // Fetch all users from the database
+      const users = await db.user.findMany({
+        select: {
+          id: true,
+          username: true,
+          email: true,
+          // Include other fields you want to return, but exclude sensitive data like passwords
+        }
+      });
+      // Respond with the list of users using users variable
+      return NextResponse.json({ users }); // Using the users variable here
+    } catch (error) {
+      // Handle any errors that occur during the process
+      return NextResponse.json({ message: "something went wrong", error: "err" });
+    }
 }
