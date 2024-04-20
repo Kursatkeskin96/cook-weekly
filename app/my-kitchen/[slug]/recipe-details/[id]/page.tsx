c1f018572608f230a40666b60049c45756f4e209
Kursat Keskin <kursatkeskinn@gmail.com>
2024-04-20 01:09:43 +0100

    bug fix

--- app/my-kitchen/[slug]/recipe-details/[id]/page.tsx
+++ app/my-kitchen/[slug]/recipe-details/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -6,9 +7,24 @@ import Link from "next/link";
 import ReactTimeAgo from "react-time-ago";
 import en from "javascript-time-ago/locale/en";
 import TimeAgo from "javascript-time-ago";
+
 TimeAgo.addDefaultLocale(en);
 
-const getMealData = async (id) => {
+interface Ingredient {
+  id: number;
+  name: string;
+  unit: string;
+  amount: string;
+}
+
+interface Meal {
+  id: number;
+  name: string;
+  createdAt: string;
+  ingredients: Ingredient[];
+}
+
+const getMealData = async (id: string) => {
   let domain;
   let protocol = "http://"; // Default protocol for local development
 
@@ -33,9 +49,8 @@ export default function MyKitchen({ params, searchParams }) {
   const { id } = params;
   const { data: session } = useSession();
   const router = useRouter();
-  const [user, setUser] = useState(null);
-  const [meal, setMeal] = useState([]);
-  const [error, setError] = useState(null);
+  const [meal, setMeal] = useState<Meal | null>(null);
+  const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -51,7 +66,7 @@ export default function MyKitchen({ params, searchParams }) {
       });
   }, [id]);
 
-  const deleteMeal = async (mealId) => {
+  const deleteMeal = async (mealId: number) => {
     try {
       const response = await fetch(`/api/recipe/${mealId}`, {
         method: "DELETE",
@@ -61,9 +76,11 @@ export default function MyKitchen({ params, searchParams }) {
         throw new Error("Failed to delete the meal");
       }
 
-      // Remove the deleted meal from the state to update the UI
-      const updatedMeals = meal.filter((m) => m.id !== mealId);
-      setMeal(updatedMeals);
+      if (meal) {
+        // Remove the deleted meal from the state to update the UI
+        const updatedMeals = meal.ingredients.filter((m) => m.id !== mealId);
+        setMeal({...meal, ingredients: updatedMeals});
+      }
 
       console.log("Meal deleted successfully");
     } catch (error) {
@@ -72,13 +89,16 @@ export default function MyKitchen({ params, searchParams }) {
   };
 
   if (loading) {
-    return <div>Loading</div>;
+    return <div>Loading...</div>;
   }
-  const username = session?.user?.username;
+
+  if (!meal) {
+    return <div>No meal data available</div>;
+  }
 
   return (
-    <div className="lg:max-w-[80%] max-w-[90%]  mx-auto">
-      <div className="flex justify-center items-center gap-5 mt-10 ">
+    <div className="lg:max-w-[80%] max-w-[90%] mx-auto">
+      <div className="flex justify-center items-center gap-5 mt-10">
         <hr className="w-[80%] h-[1px] mx-auto my-4 bg-[#FFC107] border-0 rounded"></hr>
         <div className="text-4xl text-[#610000] lg:w-96 cookie-regular text-center">
           {meal.name}&apos;s Details
@@ -86,9 +106,9 @@ export default function MyKitchen({ params, searchParams }) {
         <hr className="w-[80%] h-[1px] mx-auto my-4 bg-[#FFC107] border-0 rounded"></hr>
       </div>
       <div className="flex justify-center items-center flex-wrap">
-        <div className="lg:max-w-[55%] text-center mt-10  bg-[#1D2A2D] border-[1px] text-white border-[#3C4F76] w-[350px] h-fit py-2 rounded-[16px]">
+        <div className="lg:max-w-[55%] text-center mt-10 bg-[#1D2A2D] border-[1px] text-white border-[#3C4F76] w-[350px] h-fit py-2 rounded-[16px]">
           <h2 className="text-2xl text-[#FFC107] font-bold mb-5">{meal.name}</h2>
-          {meal.ingredients && meal.ingredients.length > 0 ? (
+          {meal.ingredients.length > 0 ? (
             <ul className="flex justify-center items-center gap-2 flex-col">
               {meal.ingredients.map((ingredient) => (
                 <li key={ingredient.id}>
@@ -96,35 +116,29 @@ export default function MyKitchen({ params, searchParams }) {
                   <span className="ml-3 text-sm text-gray-300">{ingredient.amount}</span>{" "}
                   <span className="text-xs text-gray-400">{ingredient.unit}</span>
                 </li>
-                
               ))}
             </ul>
           ) : (
             <p>No ingredients listed.</p>
           )}
-<p className="text-xs mt-5">Created: 
-  <span className="text-gray-300 ml-2">
-    {meal.createdAt ? (
-      <ReactTimeAgo date={new Date(meal.createdAt)} locale="en-US" />
-    ) : (
-      "Date not available"
-    )}
-  </span>
-</p>
-
- <div className="flex justify-center mt-5 text-sm gap-4 text-center items-center ">
-                <Link href={`/edit-recipe/${meal.id}`}>
-                  <div className="bg-[#D34C26] text-white p-1 w-20 rounded-lg cursor-pointer">
-                    Edit
-                  </div>
-                </Link>
-                <div
-                  className="bg-red-700 text-white p-1 w-20 rounded-lg cursor-pointer"
-                  onClick={() => deleteMeal(meal.id)}
-                >
-                  Delete
-                </div>
-              </div>
+          <p className="text-xs mt-5">Created: 
+            <span className="text-gray-300 ml-2">
+              <ReactTimeAgo date={new Date(meal.createdAt)} locale="en-US" />
+            </span>
+          </p>
+          <div className="flex justify-center mt-5 text-sm gap-4 text-center items-center ">
+            <Link href={`/edit-recipe/${meal.id}`}>
+              <a className="bg-[#D34C26] text-white p-1 w-20 rounded-lg cursor-pointer">
+                Edit
+              </a>
+            </Link>
+            <button
+              className="bg-red-700 text-white p-1 w-20 rounded-lg cursor-pointer"
+              onClick={() => deleteMeal(meal.id)}
+            >
+              Delete
+            </button>
+          </div>
         </div>
       </div>
     </div>
